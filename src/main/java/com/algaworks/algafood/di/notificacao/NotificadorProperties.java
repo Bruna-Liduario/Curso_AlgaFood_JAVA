699438f22afabee7af473713cb66f9f4e2454b68
Bruna Liduario de Moraes <bruna.moraes@NTBERG.ergbh.intra>
2023-11-15 18:51:16 -0300

    Primeiro Commit com classes exemplos

---
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorProperties.java
@@ -0,0 +1,30 @@
+package com.algaworks.algafood.di.notificacao;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("noticiador.email")
+public class NotificadorProperties {
+ 
+	//Host do servidor de email
+	private String hostServidor;
+	
+	//Porta do Servidor de email
+	private Integer portaServidor;
+	
+	public String getHostServidor() {
+		return hostServidor;
+	}
+	public void setHostServidor(String hostServidor) {
+		this.hostServidor = hostServidor;
+	}
+	public Integer getPortaServidor() {
+		return portaServidor;
+	}
+	public void setPortaServidor(Integer portaServidor) {
+		this.portaServidor = portaServidor;
+	}
+	
+	
+}
